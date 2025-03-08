4991898ed8cf9b7e861ffd131c7ca8b902f0f11e
dipankrr <dipankar.laptop@gmail.com>
2025-03-08 20:45:16 +0530

    added firebase web

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_notes_app/pages/homepage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  if (kIsWeb){
+    await Firebase.initializeApp(options: const FirebaseOptions(apiKey: "AIzaSyC8qeFl8tk1cLacVyAFUktf93MGEIokTd4",
+        authDomain: "note-app-a4fe8.firebaseapp.com",
+        projectId: "note-app-a4fe8",
+        storageBucket: "note-app-a4fe8.firebasestorage.app",
+        messagingSenderId: "921624388283",
+        appId: "1:921624388283:web:12b6f953881251880aff9e",
+        measurementId: "G-N7VZ2L5TFV"));
+  }
+  else { await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
